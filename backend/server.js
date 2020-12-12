e8af3969357b55a33260a2f0130be320006c0377
ahmaat19 <ahmaat19@gmail.com>
2020-12-12 14:08:31 +0300

    added build runner

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,20 @@ app.use(express.json())
 
 app.use('/api/users', userRoutes)
 
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.use(notFound)
 
 app.use(errorHandler)
